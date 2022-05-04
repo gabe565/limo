108220c4553699d0de5f33f09c9863ffaad04da6
Gabe Cook <gabe565@gmail.com>
2022-05-03 23:37:34 -0500

    :loud_sound: Add listen log

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,11 +1,13 @@
 package server
 
 import (
+	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
 type Server struct{}
 
 func (s *Server) ListenAndServe(addr string) error {
+	log.WithField("address", addr).Info("listening for requests")
 	return http.ListenAndServe(addr, s.Handler())
 }
